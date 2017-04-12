b4a751aafdac5397018a9965126df60099a30730
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-04-12 15:28:06 -0300

    implementando codigos

--- main.c
+++ main.c
@@ -1,8 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct no {
+	float valor;
+	struct no *proximoNo;
+}nos;
+
+typedef nos *controlaNos;
+
+void printPause();//Pausa a execução e limpa a tela
+void deposito(controlaNos *topo, int empilha);//Empilha valores
+void sque(controlaNos *topo); //Desempilha valores
+void imprime(controlaNos topo); //Imprime a pilha
+int vazio(controlaNos topo); //Verifica se a pilha esta vazia
 
 int main(int argc, char *argv[]) {
-	return 0;
+	controlaNos topoPilha = NULL;
+    int menu = 0, num = 0;
+    float vlrDeposito = 0;
+    
+    while (menu != 5) {
+        printf("Escolha uma opcao:\n"
+               "1 - Deposito\n"
+               "2 - Saque\n"
+               "3 - Extrato\n"
+               "4 - Relatorios\n"
+			   "5 - Encerrar sessao\n");
+        scanf("%d",&menu);
+        system("cls");
+        switch (menu) {
+            case 1:
+                printf("Digite o valor do deposito:\n");
+                scanf("%f",&vlrDeposito);
+                system("cls");
+                deposito(&topoPilha, vlrDeposito);
+                break;
+            case 2:
+                if (vazio(topoPilha)) {
+                    printf("Nao ha nada para desempilhar!\n");
+                    printPause();
+                }
+                else {
+                    saque(&topoPilha);
+                }
+                break;
+            case 3:
+				if(vazio(topoPilha)){
+					printf("A pilha esta vazia!\n");
+				}else {
+					imprime(topoPilha);
+				}   
+				printPause();
+				break; 
+            default:
+                printf("Opcao invalida!\n");
+                printPause();
+                break;
+        }
+    }
+    
+    system("pause");
+    return 0;
 }
+
+//função que pause e limpa a tela
+void printPause(){
+	system("pause");//comando que pausa tela
+	system("cls");//comando que limpa a tela
+}
+
+//Esta função vai tirar o último nó da pilha, e retirá-lo de lá
+void saque(controlaNos *topo){
+	controlaNos popNo = NULL;
+	
+	popNo = *topo;
+	*topo = (*topo)->proximoNo;
+	free(popNo);
+}
+
+//função que vai inserir elementos na pilha
+void deposito(controlaNos *topo, int empilha){
+	controlaNos novoNo = NULL;
+	
+	novoNo = (controlaNos) malloc(sizeof(nos));
+	
+	if(novoNo == NULL){
+		printf("O valor nao foi depositado!\n");
+		printPause();
+	}else {
+		novoNo->valor=empilha;
+		novoNo->proximoNo = *topo;
+		*topo=novoNo; 
+	}
+}
+
+//função que verefica se a função está vazia
+int vazio(controlaNos topo){
+	return (topo == NULL);
+}
+
+//função que exibe os valores na pilha
+void imprime(controlaNos topo){
+	while(topo != NULL){
+		printf("-->%.2f\n", topo->valor);
+		topo=topo->proximoNo;
+	}
+} 
